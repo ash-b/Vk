2543adbf857ca5900fff31df70fd39fc9c225e38
Ashish Bubne <ashish.bubne@itsyuj.com>
2017-12-28 21:25:57 +0530

    streams added

---
+++ backend/views/stream/_form.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Stream */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="stream-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+    <div class="col-md-12">
+    <div class="col-md-6">
+        
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'status')->dropDownList(array(1 => "Yes", 0 => "No")) ?>
+    </div>
+    <div class="col-md-6">
+    </div>
+    </div>
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success pull-right' : 'btn btn-primary pull-right']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/views/stream/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\StreamSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Streams';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="stream-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p class="text-right">
+        <?= Html::a('Create Stream', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+<?php Pjax::begin(); ?>    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'description:ntext',
+            [
+                'attribute' => 'status',
+                'value' => function ($model) {
+                    return $model->status ? 'Yes' : 'No';
+                },
+                'filter' => array(1 => "Yes", 0 => "No"),
+            ],
+            'created_at',
+            // 'updated_at',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+<?php Pjax::end(); ?></div>
